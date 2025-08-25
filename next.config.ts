c1f43bc9cc0aaecd565c0828f8d480d7c2e9669f
BitByte08 <me@bitworkspace.kr>
2025-08-26 07:55:13 +0900

    feat :: PWA 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+import withPWA from "next-pwa";
+const nextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA({dest: "public",
+                        register: true,
+                        skipWaiting: true}
+)(nextConfig);
