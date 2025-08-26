29a927ee012fd4a5a7f0e2ca8cfcbf155b047808
BitByte08 <me@bitworkspace.kr>
2025-08-26 15:14:04 +0900

    feat :: main에 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import MainContainer from "@/shared/components/containers/MainContainer";
+import WeeklyCalendar from "@/features/home/components/WeeklyCalendar";
+import SearchBar from "@/features/home/components/SearchBar";
+
 export default function Home() {
   return (
-    <>
+    <MainContainer>
+      <WeeklyCalendar />
+      <SearchBar />
       Main
-    </>
+    </MainContainer>
   );
 }
