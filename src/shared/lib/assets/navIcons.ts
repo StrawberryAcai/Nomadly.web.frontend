a54bf7c24503ba4cd0668c25abef7b49efe7038a
BitByte08 <me@bitworkspace.kr>
2025-08-26 12:34:17 +0900

    feat :: 에셋 추가

---
+++ src/shared/lib/assets/navIcons.ts
@@ -0,0 +1,7 @@
+import Home from "@public/icons/home.svg";
+import Explore from "@public/icons/explore.svg";
+import Tour from "@public/icons/tour.svg";
+import Community from "@public/icons/community.svg";
+import Profile from "@public/icons/profile.svg";
+
+export { Home, Explore, Tour, Community, Profile };
