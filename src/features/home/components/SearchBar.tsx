54cf8bf1db10c08d638c1563b4dbecdc4e14f4c5
BitByte08 <me@bitworkspace.kr>
2025-08-26 15:12:50 +0900

    feat :: Text input 구현

---
+++ src/features/home/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SectionContainer from "@/shared/components/containers/SectionContainer";
+import TextInput from "@/shared/components/inputs/TextInput";
+import Search from "@public/icons/search.svg";
+const SearchBar: React.FC = () => {
+  return (
+    <SectionContainer className="h-16">
+      <TextInput src={Search} alt="검색" placeholder="일정을 검색해 보세요." />
+    </SectionContainer>
+  )
+}
+
+export default SearchBar;
