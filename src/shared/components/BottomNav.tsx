8fe634cf19be6b5b78b5b95dca0405aa2dfa56cc
BitByte08 <me@bitworkspace.kr>
2025-08-26 12:34:46 +0900

    feat :: 네비게이션바 구현

---
+++ src/shared/components/BottomNav.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React from "react";
+import {StaticImport} from "next/dist/shared/lib/get-img-props";
+import Link from "next/link";
+import Image from "next/image";
+import {Home, Explore, Tour, Community, Profile} from "@/shared/lib/assets/navIcons";
+
+interface BottomNavIconProps {
+  href: string;
+  src: string | StaticImport;
+  alt: string;
+}
+
+const BottomNavIcon: React.FC<BottomNavIconProps> = ({href, src, alt}) => (
+  <Link href={href} className="flex-1 flex flex-col items-center">
+    <Image src={src} alt={alt} className="h-6 w-6" />
+    <span className="text-body-md">{alt}</span>
+  </Link>
+)
+
+const BottomNav: React.FC = () => {
+  return (
+    <nav className="h-16 w-full fixed bottom-0 border-outline border-t-1 flex justify-around px-4 py-2">
+      <BottomNavIcon href="/" src={Home} alt="홈" />
+      <BottomNavIcon href="/explore" src={Explore} alt="지역체험" />
+      <BottomNavIcon href="/tour" src={Tour} alt="관광" />
+      <BottomNavIcon href="/community" src={Community} alt="커뮤니티" />
+      <BottomNavIcon href="/Profile" src={Profile} alt="마이" />
+    </nav>
+  )
+}
+
+export default BottomNav;
