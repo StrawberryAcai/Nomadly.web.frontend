fb7a3c46d6f0fe5c2abb239e9c557e627dfa0e34
BitByte08 <me@bitworkspace.kr>
2025-08-26 17:53:48 +0900

    feat :: main에 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import MainContainer from "@/shared/components/containers/MainContainer";
 import WeeklyCalendar from "@/features/home/components/WeeklyCalendar";
 import SearchBar from "@/features/home/components/SearchBar";
+import AiScheduleButton from "@/features/home/components/AiScheduleButton";
+import ShortcutButtons from "@/features/home/components/ShortcutButtons";
 
 export default function Home() {
   return (
     <MainContainer>
       <WeeklyCalendar />
       <SearchBar />
-      Main
+      <AiScheduleButton />
+      <ShortcutButtons />
     </MainContainer>
   );
 }
