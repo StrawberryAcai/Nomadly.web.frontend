6065ad82e7ce59f1683b456c8ac93041e3823f2c
BitByte08 <me@bitworkspace.kr>
2025-08-26 17:52:26 +0900

    feat :: 이미지 적용

--- src/features/home/components/AiScheduleButton.tsx
+++ src/features/home/components/AiScheduleButton.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import SectionContainer from "@/shared/components/containers/SectionContainer";
 import Link from "next/link";
 import Image from "next/image";
+import {Btn2xBg} from "@/shared/lib/assets/backgrounds";
+import {AI} from "@/shared/lib/assets/icons";
 
 const AiScheduleButton: React.FC = () => {
   return (
     <SectionContainer className="h-32">
-      <Link href="/" className="">
-        버튼 들어가는 자리
+      <Link href="/" className="w-full h-full relative bg-center rounded-xl px-3 py-8" style={{ backgroundImage: `url(${Btn2xBg.src})`, backgroundSize: "cover" }}>
+        <span className="text-white text-body-lg">AI로<br/>일정 생성하기</span>
+        <Image src={AI} alt="simbol" className="absolute right-0 top-0 bottom-0" />
       </Link>
     </SectionContainer>
   )
---
+++ src/shared/lib/assets/backgrounds.ts
@@ -0,0 +1,3 @@
+import Btn2xBg from "@public/backgrounds/button2x.svg";
+import Btn1xBg from "@public/backgrounds/button1x.svg";
+export {Btn2xBg, Btn1xBg}
