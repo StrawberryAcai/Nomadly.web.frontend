1a514ea3aa0e8ea22ade937d295ddf180d679ce3
BitByte08 <me@bitworkspace.kr>
2025-08-28 08:03:38 +0900

    feat :: 다른 크기 구현

--- src/shared/components/containers/CardContainer.tsx
+++ src/shared/components/containers/CardContainer.tsx
@@ -11,14 +11,14 @@ interface CardContainerProps {
 const CardContainer: React.FC<CardContainerProps> = ({title, score, bookmark, like, url, isLarge}) => {
 
   return (
-    <article className="aspect-[3/4] w-[12.5rem] bg-black bg-center bg-cover p-3 flex flex-col justify-between rounded-xl flex-none"
+    <article className={(isLarge?"aspect-[3/4] w-[12.5rem]":"aspect-square w-[9.375rem]")+" bg-black bg-center bg-cover p-3 flex flex-col justify-between rounded-xl flex-none"}
              style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${url})`}}>
       <header className="flex flex-col">
         <span className="text-body-lg text-white">{title}</span>
         <span className="text-caption text-secondary">{score}  ·  {"방문객 증가 중"}</span>
       </header>
       <footer className="">
-        북마크랑 좋아요 위치
+        123
       </footer>
     </article>
   )
