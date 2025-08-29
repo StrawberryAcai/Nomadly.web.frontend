cdde3e6eccaf289d7a799ed389eef3ed2eca002b
BitByte08 <me@bitworkspace.kr>
2025-08-29 15:07:03 +0900

    fix :: ESLint 에러 수정

--- src/shared/hooks/useKakaoLoader.ts
+++ src/shared/hooks/useKakaoLoader.ts
@@ -2,7 +2,7 @@ import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk";
 
 const useKakaoLoader = () => {
   useKakaoLoaderOrigin({
-    appkey: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
+    appkey: process.env.NEXT_PUBLIC_KAKAO_API_KEY || "API_KEY",
     libraries: ["clusterer", "drawing", "services"]
   })
 }
