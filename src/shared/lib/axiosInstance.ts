fddd84e94af287e94fe153acab20d4d27015c88a
BitByte08 <me@bitworkspace.kr>
2025-09-01 15:44:02 +0900

    feat :: axiosInstance 생성 && JWT 토큰 갱신

---
+++ src/shared/lib/axiosInstance.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+let accessToken: string | null = null;
+
+export const setAccessToken = (token: string) => accessToken = token;
+
+export const getAccessToken = () => accessToken;
+
+const api = axios.create({
+  baseURL: "http://localhost:8000",
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      try {
+        const res = await axios.post(
+          "http://localhost:8000/api/auth/reissue",
+          {},
+          { withCredentials: true }
+        );
+        const newToken = res.headers["authorization"]?.replace("Bearer ", "");
+        if (newToken) {
+          setAccessToken(newToken);
+          error.config.headers["Authorization"] = `Bearer ${newToken}`;
+          return api.request(error.config);
+        }
+      } catch (refreshError) {
+        console.error("토큰 갱신 실패:", refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
