5817ed0251baa973a5999a98b465b0edb254894a
BitByte08 <me@bitworkspace.kr>
2025-09-02 00:06:00 +0900

    fix :: 지도 크기 수정

--- src/app/tour/page.tsx
+++ src/app/tour/page.tsx
@@ -5,20 +5,17 @@ import useKakaoLoader from "@/shared/hooks/useKakaoLoader";
 export default function Page() {
   useKakaoLoader();
   return (
-    <Map // 지도를 표시할 Container
-      id="map"
-      center={{
-        // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
-      }}
-      style={{
-        // 지도의 크기
-        width: "100%",
-        height: "100%",
-      }}
-      level={3} // 지도의 확대 레벨
-
-    />
+    <section className="h-[calc(100vh-4rem)] w-full">
+      <Map // 지도를 표시할 Container
+        id="map"
+        center={{
+          // 지도의 중심좌표
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+        level={3} // 지도의 확대 레벨
+        className="h-full w-full"
+      />
+    </section>
   )
 }
