2e55a4857e4a57c41e41721bb6a3669e30a33f15
BitByte08 <me@bitworkspace.kr>
2025-09-01 15:44:46 +0900

    fix :: 지도 크기 수정

--- src/app/tour/page.tsx
+++ src/app/tour/page.tsx
@@ -15,9 +15,10 @@ export default function Page() {
       style={{
         // 지도의 크기
         width: "100%",
-        height: "350px",
+        height: "100%",
       }}
       level={3} // 지도의 확대 레벨
+
     />
   )
 }
