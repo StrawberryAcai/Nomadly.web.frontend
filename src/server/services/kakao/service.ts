2b83373c814090147d67a79d266380b1e279f77c
BitByte08 <me@bitworkspace.kr>
2025-09-03 00:32:53 +0900

    feat :: 지역 정규화

--- src/server/services/kakao/service.ts
+++ src/server/services/kakao/service.ts
@@ -1,11 +1,12 @@
-// src/server/services/kakao/service.ts
 import kakaoInstance from "./client";
 import { KakaoRegionResponse } from "./dto";
+
+const normalizeAddress = (address: string) => address.replace(/(광역시|특별시)/, "");
 
 export async function getRegion(lat: number, lon: number) {
   const res = await kakaoInstance.get<KakaoRegionResponse>(
     "/geo/coord2regioncode.json",
     { params: { x: lon, y: lat, input_coord: "WGS84" } }
   );
-  return res.data.documents[0].address_name;
+  return normalizeAddress(res.data.documents[0].region_1depth_name);
 }
