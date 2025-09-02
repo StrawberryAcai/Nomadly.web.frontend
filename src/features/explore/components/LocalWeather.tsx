4e31d686e57addb738324283120a970342085d50
BitByte08 <me@bitworkspace.kr>
2025-09-03 00:32:40 +0900

    feat :: 컴포넌트 수정

--- src/features/explore/components/LocalWeather.tsx
+++ src/features/explore/components/LocalWeather.tsx
@@ -1,51 +1,39 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import SectionContainer from "@/shared/components/containers/SectionContainer";
 
 export default function LocalWeather() {
   const [address, setAddress] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    if (!navigator.geolocation) {
-      setError('브라우저에서 위치 정보를 지원하지 않습니다.');
-      return;
-    }
-
+    if (!navigator.geolocation) return;
     navigator.geolocation.getCurrentPosition(
       async (position) => {
         try {
           const { latitude, longitude } = position.coords;
           const res = await fetch(`/api/kakao/region?lat=${latitude}&lon=${longitude}`);
           const data = await res.json();
-
-          if (data.error) {
-            setError(data.error);
-          } else {
-            setAddress(data.addressName);
-            console.log('Region data:', data.addressName);
-          }
+          if(!data.error) setAddress(data.addressName);
         } catch (err) {
           console.error(err);
-          setError('지역 정보를 가져오는 데 실패했습니다.');
         }
       },
       (err) => {
         console.error(err);
-        setError('위치 정보 제공을 허용하지 않았습니다.');
       }
     );
   }, []);
 
   return (
-    <div className="p-4 bg-gray-50 border rounded-md">
-      {error ? (
-        <p className="text-red-500">{error}</p>
-      ) : address ? (
-        <p>현재 위치: {address}</p>
-      ) : (
-        <p>위치 정보를 가져오는 중...</p>
-      )}
-    </div>
+    <SectionContainer className="justify-between">
+      <div className="flex items-end gap-1">
+        <h3>{address}</h3>
+        <span className="text-caption text-secondary">반경 3km</span>
+      </div>
+      <div>
+        날씨
+      </div>
+    </SectionContainer>
   );
 }
