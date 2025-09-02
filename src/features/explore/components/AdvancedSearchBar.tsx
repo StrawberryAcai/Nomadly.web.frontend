7f27b99f9cd9e94b9c5af52c305b737bed4b1aec
BitByte08 <me@bitworkspace.kr>
2025-09-02 23:06:39 +0900

    feat :: Select 적용

--- src/features/explore/components/AdvancedSearchBar.tsx
+++ src/features/explore/components/AdvancedSearchBar.tsx
@@ -3,14 +3,18 @@ import SectionContainer from "@/shared/components/containers/SectionContainer";
 import TextInput from "@/shared/components/inputs/TextInput";
 
 import Search from "@public/icons/search.svg";
+import Select from "@/shared/components/inputs/Select";
 
 const AdvancedSearchBar = () => {
+  const Category: string[] = ["a", "b", "c", "d"];
   return (
-    <SectionContainer>
+    <SectionContainer className="flex-wrap gap-3">
       <TextInput src={Search} alt={"Place"} placeholder={"장소를 검색해보세요"} />
-      <select>
-        <option></option>
-      </select>
+      <div className="flex gap-3">
+        <Select placeholder="카테고리" items={Category}/>
+        <Select placeholder="가격대" items={Category}/>
+        <Select placeholder="지역" items={Category}/>
+      </div>
     </SectionContainer>
   )
 }
