7529a1ab0f42b8d22a38b5b520f55f8e28b7b516
BitByte08 <me@bitworkspace.kr>
2025-09-05 03:27:24 +0900

    feat :: NavBar 스타일 적용

--- src/features/profile/components/NavBar.tsx
+++ src/features/profile/components/NavBar.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Link from "next/link";
 
-const NavBar = () => (
-  <nav>
-    <Link href="/profile">저장된 일정</Link>
-    <Link href="/profile/bookmark">북마크한 장소</Link>
-    <Link href="/profile/reservation">예약 내역</Link>
-    <Link href="/profile/community">커뮤니티 활동</Link>
+interface CustomLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+const CustomLink: React.FC<CustomLinkProps> = ({href, children}) => <Link href={href} className="py-1 text-body-md">{children}</Link>
+const NavBar: React.FC = () => (
+  <nav className="px-4 flex justify-around">
+    <CustomLink href="/profile">저장된 일정</CustomLink>
+    <CustomLink href="/profile/bookmark">북마크한 장소</CustomLink>
+    <CustomLink href="/profile/reservation">예약 내역</CustomLink>
+    <CustomLink href="/profile/community">커뮤니티 활동</CustomLink>
   </nav>
 )
 
