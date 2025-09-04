627e1eab8c5fa320448f13952c3ce74cfc971cd1
BitByte08 <me@bitworkspace.kr>
2025-09-05 03:55:58 +0900

    feat :: 타입지정

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import TopBar from "@/shared/components/TopBar";
 import Image from "next/image";
 import Setting from "@public/icons/button/setting.svg";
@@ -5,8 +6,7 @@ import ProfileCard from "@/features/profile/components/ProfileCard";
 import MainContainer from "@/shared/components/containers/MainContainer";
 import NavBar from "@/features/profile/components/NavBar";
 
-
-export default function layout({children}) {
+export default function layout({children}: {children: React.ReactNode}) {
   return (
     <MainContainer>
       <TopBar>
