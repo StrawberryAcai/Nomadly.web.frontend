c31e7cc00a3dc8f917b7c7694d1c27ffc23e0804
BitByte08 <me@bitworkspace.kr>
2025-09-05 01:52:50 +0900

    feat :: 커스텀 Input onChange Props 추가

--- src/shared/components/inputs/TextInput.tsx
+++ src/shared/components/inputs/TextInput.tsx
@@ -7,12 +7,13 @@ interface TextInputProps {
   alt: string;
   placeholder: string;
   ref?: React.Ref<HTMLInputElement>
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
-const TextInput: React.FC<TextInputProps> = ({src, alt, placeholder, ref}) => {
+const TextInput: React.FC<TextInputProps> = ({src, alt, placeholder, ref, onChange}) => {
   return (
     <div className="h-full w-full flex p-3 border-outline border-1 box-border rounded-xl gap-1">
       <Image src={src} alt={alt} />
-      <input type="text" placeholder={placeholder} ref={ref} className="flex-1 focus:outline-none" />
+      <input type="text" placeholder={placeholder} ref={ref} className="flex-1 focus:outline-none" onChange={onChange} />
     </div>
   )
 }
