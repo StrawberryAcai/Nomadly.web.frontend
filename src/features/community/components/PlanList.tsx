f506bfe9274d786f7ee63f9763b945e98cfbdfcc
BitByte08 <me@bitworkspace.kr>
2025-09-05 03:20:06 +0900

    delete :: 사용하지 않는 상수 제거

--- src/features/community/components/PlanList.tsx
+++ src/features/community/components/PlanList.tsx
@@ -6,7 +6,6 @@ interface PlanListProps {
   data: PlanListResponse
 }
 const PlanList: React.FC<PlanListProps> = ({data}) => {
-  const tempUrl = "https://i.namu.wiki/i/Z41qK_Jp4TYWr7IaMOcgrRmtTF_F7qWX5ugdrTDjAHZPkvrf8ahJZYmWC-6cmaS1kgPrsV4UgzgxVNigvN9Uml2-5Vq5Oa-LLuNdMqAglZs1pG7ArNSN2Mzsvdewm5KjCTfJdteQyYDGl9njSF6_WQ.webp";
   return (
     <section className="flex flex-col gap-4">
       {data?.plans.map((plan) => <PlanItem key={plan.plan_id} {...plan} />)}
