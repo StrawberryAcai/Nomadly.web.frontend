bd5ece7a6d94b61ed57c7851fa9bd2f3c068130c
BitByte08 <me@bitworkspace.kr>
2025-09-05 01:20:15 +0900

    feat :: 북마크 버튼 선택 효과 추가

--- src/shared/components/inputs/BookmarkButton.tsx
+++ src/shared/components/inputs/BookmarkButton.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Image from "next/image";
 
-import Bookmark from '@public/icons/button/bookmark.svg';
+import Bookmark from '@public/icons/button/bookmark/bookmark.svg';
+import BookmarkP from '@public/icons/button/bookmark/bookmark_p.svg';
 
 const formatCount = (count: number): string => {
   if (count < 1000) return count.toString();
@@ -12,12 +13,13 @@ const formatCount = (count: number): string => {
 interface BookmarkButtonProps {
   onClick?: () => void;
   count: number;
+  isBookmark: boolean;
 }
-const BookmarkButton: React.FC<BookmarkButtonProps> = ({onClick, count}) => {
+const BookmarkButton: React.FC<BookmarkButtonProps> = ({onClick, count, isBookmark}) => {
   return (
     <div onClick={onClick} className="h-8 flex flex-row gap-1 py-2 items-center">
-      <Image src={Bookmark} alt="Bookmark icon" />
-      <span className="text-caption text-secondary">{formatCount(count)}</span>
+      <Image src={isBookmark? BookmarkP : Bookmark} alt="Bookmark icon" />
+      <span className={`text-caption ${isBookmark ? "text-[#EAB308]" : "text-secondary"}`}>{formatCount(count)}</span>
     </div>
   )
 }
