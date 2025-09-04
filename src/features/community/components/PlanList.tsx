307332248b10768dacdeee1ffcda731cccfc7895
BitByte08 <me@bitworkspace.kr>
2025-09-05 01:53:05 +0900

    feat :: map 렌더링

--- src/features/community/components/PlanList.tsx
+++ src/features/community/components/PlanList.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PlanItem from "@/features/community/components/PlanItem";
+import {PlanListResponse} from "@/features/community/api/dto";
 
-
-const ReviewList: React.FC = () => {
+interface PlanListProps {
+  data: PlanListResponse
+}
+const PlanList: React.FC<PlanListProps> = ({data}) => {
+  const tempUrl = "https://i.namu.wiki/i/Z41qK_Jp4TYWr7IaMOcgrRmtTF_F7qWX5ugdrTDjAHZPkvrf8ahJZYmWC-6cmaS1kgPrsV4UgzgxVNigvN9Uml2-5Vq5Oa-LLuNdMqAglZs1pG7ArNSN2Mzsvdewm5KjCTfJdteQyYDGl9njSF6_WQ.webp";
   return (
     <section className="flex flex-col gap-4">
-      <PlanItem id={1} url={"https://i.namu.wiki/i/Z41qK_Jp4TYWr7IaMOcgrRmtTF_F7qWX5ugdrTDjAHZPkvrf8ahJZYmWC-6cmaS1kgPrsV4UgzgxVNigvN9Uml2-5Vq5Oa-LLuNdMqAglZs1pG7ArNSN2Mzsvdewm5KjCTfJdteQyYDGl9njSF6_WQ.webp"}
-                title={"여기 감자탕 너무 맛있어요!"} content={"test"} like={1000} isLiked={true} bookmark={230} isBookmarked={false} />
+      {data?.plans.map((plan) => <PlanItem key={plan.plan_id} {...plan} url={tempUrl} />)}
     </section>
   )
 }
 
-export default ReviewList;
+export default PlanList;
