092eb1e6da78a77f1822c1f14ac79e68e3d216ec
BitByte08 <me@bitworkspace.kr>
2025-09-05 03:20:59 +0900

    feat :: Profile Card 구현

---
+++ src/features/profile/components/ProfileCard.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from "next/image";
+
+import RightArrow from '@public/icons/arrow/right_arrow.svg';
+
+const ProfileCard: React.FC = () => {
+  const tempUrl = "https://i.namu.wiki/i/Z41qK_Jp4TYWr7IaMOcgrRmtTF_F7qWX5ugdrTDjAHZPkvrf8ahJZYmWC-6cmaS1kgPrsV4UgzgxVNigvN9Uml2-5Vq5Oa-LLuNdMqAglZs1pG7ArNSN2Mzsvdewm5KjCTfJdteQyYDGl9njSF6_WQ.webp";
+  return (
+    <section className="pt-1 px-4">
+      <div className="h-18 border-outline border-1 rounded-xl p-3 flex gap-2 items-center">
+        <Image src={tempUrl} alt="img" width={48} height={48} className="rounded-full max-w-12 min-w-12 max-h-12 min-h-12" />
+        <div className="flex-1 flex flex-col">
+          <span className="text-body-md">유저이름</span>
+          <span className="text-caption text-secondary">17세 · 대한민국 부산</span>
+        </div>
+        <Image src={RightArrow} alt="right_arrow" />
+      </div>
+    </section>
+  )
+}
+
+export default ProfileCard;
