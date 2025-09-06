cc5b2ee7449783a41473eca6719423aa75860d49
BitByte08 <me@bitworkspace.kr>
2025-09-07 02:07:51 +0900

    feat :: 프로필 관련 api 연결코드 추가

--- src/features/profile/api/queries.ts
+++ src/features/profile/api/queries.ts
@@ -1,7 +1,24 @@
 import api from '@/shared/lib/axiosInstance';
 import { ProfileDto } from './dto';
+import {PlanListResponse} from "@/features/community/api/dto";
+import {dummyPlanData} from "@/features/community/api/dummy";
 
 export const getProfile = async (): Promise<ProfileDto> => {
   const res = await api.get<ProfileDto>("/api/users/profile");
   return res.data;
 };
+
+export const getMePlan = async (): Promise<PlanListResponse> => {
+  // await api.get<PlanListResponse>("/api/me/plans");
+  return dummyPlanData
+}
+
+export const getMeBookmarkedPlan = async (): Promise<PlanListResponse> => {
+  // await api.get<PlanListResponse>("/api/me/bookmark/plans");
+  return dummyPlanData
+}
+
+export const getMePublicPlan = async (): Promise<PlanListResponse> => {
+  // await api.get<PlanListResponse>("/api/me/board/plans");
+  return dummyPlanData
+}
