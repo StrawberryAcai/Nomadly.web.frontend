5e2be2fdf9036569541be69737a895ab21a3506b
BitByte08 <me@bitworkspace.kr>
2025-09-07 02:09:37 +0900

    feat :: 경로 이동

--- src/features/home/components/AiScheduleButton.tsx
+++ src/features/home/components/AiScheduleButton.tsx
@@ -9,7 +9,7 @@ import AI from "@public/icons/button/AI.svg";
 const AiScheduleButton: React.FC = () => {
   return (
     <SectionContainer className="h-32">
-      <Link href="/" className="w-full h-full relative bg-center rounded-xl px-3 py-8" style={{ backgroundImage: `url(${Bg.src})`, backgroundSize: "cover" }}>
+      <Link href="/schedule" className="w-full h-full relative bg-center rounded-xl px-3 py-8" style={{ backgroundImage: `url(${Bg.src})`, backgroundSize: "cover" }}>
         <span className="text-white text-body-lg">AI로<br/>일정 생성하기</span>
         <Image src={AI} alt="simbol" className="absolute right-0 top-0 bottom-0" />
       </Link>
