2a5b700216e7926f7fa92dfa03cd161375506e74
BitByte08 <me@bitworkspace.kr>
2025-09-08 00:42:53 +0900

    feat :: TextInput Props 추가

--- src/shared/components/inputs/TextInput.tsx
+++ src/shared/components/inputs/TextInput.tsx
@@ -9,10 +9,11 @@ interface TextInputProps {
   placeholder: string;
   ref?: React.Ref<HTMLInputElement>
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  className?: string;
 }
-const TextInput: React.FC<TextInputProps> = ({src, alt, placeholder, ref, onChange, type = "text"}) => {
+const TextInput: React.FC<TextInputProps> = ({src, alt, placeholder, ref, onChange, type = "text", className}) => {
   return (
-    <div className="w-full flex p-3 border-outline border-1 box-border rounded-xl gap-1">
+    <div className={`w-full flex p-3 border-outline border-1 box-border rounded-xl gap-1 ${className}`}>
       {src && alt && <Image src={src} alt={alt} />}
       <input type={type} placeholder={placeholder} ref={ref} className="flex-1 box-border focus:outline-none" onChange={onChange} required />
     </div>
