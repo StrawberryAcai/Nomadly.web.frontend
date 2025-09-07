b2dd2c6532778ec0c3d40114be8aa050aaa00568
BitByte08 <me@bitworkspace.kr>
2025-09-08 08:42:46 +0900

    delete :: 버튼 제거

--- src/app/(page)/(no-nav)/schedule/page.tsx
+++ src/app/(page)/(no-nav)/schedule/page.tsx
@@ -6,8 +6,7 @@ export default async function Page() {
     <SectionContainer className="flex-col relative flex-1">
       <h3 className="mx-auto font-semibold">일정을 어떻게 생성하시겠어요?</h3>
       <div className="flex flex-col absolute top-1/4 left-0 right-0 gap-6">
-        <Link href="/schedule/ai" className="mx-auto">AI로 빠르게 생성하기</Link>
-        <Link href="/schedule/user" className="mx-auto">직접 단계별로 만들기</Link>
+        <Link href="/schedule/user" className="mx-auto">AI로 빠르게 생성하기</Link>
       </div>
     </SectionContainer>
   )
