5169afb8fdb7862e4d13b9701c665522f0ceb5a8
BitByte08 <me@bitworkspace.kr>
2025-09-08 00:43:11 +0900

    feat :: 일정 생성 시작 페이지 퍼블리싱

--- src/app/(page)/(no-nav)/schedule/page.tsx
+++ src/app/(page)/(no-nav)/schedule/page.tsx
@@ -1,9 +1,14 @@
 import SectionContainer from "@/shared/components/containers/SectionContainer";
+import Link from "next/link";
 
 export default async function Page() {
   return(
-    <SectionContainer>
-       <h3>일정을 어떻게 생성하시겠어요?</h3>
+    <SectionContainer className="flex-col relative flex-1">
+      <h3 className="mx-auto font-semibold">일정을 어떻게 생성하시겠어요?</h3>
+      <div className="flex flex-col absolute top-1/4 left-0 right-0 gap-6">
+        <Link href="/schedule/ai" className="mx-auto">AI로 빠르게 생성하기</Link>
+        <Link href="/schedule/user" className="mx-auto">직접 단계별로 만들기</Link>
+      </div>
     </SectionContainer>
   )
 }
