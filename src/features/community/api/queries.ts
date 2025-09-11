851b24b531d8a6f79e671d5f047c8fbad4bff92f
BitByte08 <me@bitworkspace.kr>
2025-09-11 09:38:18 +0900

    feat :: 구현된 API 연동

--- src/features/community/api/queries.ts
+++ src/features/community/api/queries.ts
@@ -1,22 +1,12 @@
 import { PlanListResponse, PlanListRequest, PlanDetailResponse } from "./dto";
-import { dummyPlanData, dummyPlanDetailData } from "./dummy";
-// import api from '@/shared/lib/axiosInstance';
+import api from "@/shared/lib/axiosInstance";
 
 export const getPlanList = async (params?: PlanListRequest): Promise<PlanListResponse> => {
-  if (params?.keyword) {
-    // await api.get(`/api/board?keyword=${params?.keyword}`);
-    return {
-      plans: dummyPlanData.plans.filter((plan) =>
-        plan.title.includes(params.keyword!)
-      ),
-    };
-  }
-  // await api.get("/api/board");
-  return dummyPlanData;
+  if (params?.keyword) return await api.get(`/api/board?keyword=${params?.keyword}`);
+  return await api.get("/api/board");
 };
 
 export const getPlanDetail = async (plan_id: number | undefined): Promise<PlanDetailResponse | null> => {
   if(undefined === plan_id) return null;
-  // await api.get(`/api/board/${plan_id}}`)
-  return dummyPlanDetailData;
+  return await api.get(`/api/board/${plan_id}}`)
 };
