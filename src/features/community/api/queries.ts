ccb1e7bb6ef6399ed22a8c03a292603ce2434e71
BitByte08 <me@bitworkspace.kr>
2025-09-12 09:30:24 +0900

    fix :: return 값 수정

--- src/features/community/api/queries.ts
+++ src/features/community/api/queries.ts
@@ -2,11 +2,11 @@ import { PlanListResponse, PlanListRequest, PlanDetailResponse } from "./dto";
 import api from "@/shared/lib/axiosInstance";
 
 export const getPlanList = async (params?: PlanListRequest): Promise<PlanListResponse[]> => {
-  if (params?.keyword) return await api.get(`/api/board?keyword=${params?.keyword}`);
-  return await api.get("/api/board");
+  if (params?.keyword) return await api.get(`/api/board?keyword=${params?.keyword}`).then((res) => res.data);
+  return await api.get("/api/board").then((res) => res.data);
 };
 
 export const getPlanDetail = async (plan_id: number | undefined): Promise<PlanDetailResponse | null> => {
   if(undefined === plan_id) return null;
-  return await api.get(`/api/board/${plan_id}}`)
+  return await api.get(`/api/board/${plan_id}}`).then((res) => res.data);
 };
