665c152a95783963c8d5fceb8870c65de43c0b1e
bitbyte08 <me@bitworkspace.kr>
2025-09-17 15:27:28 +0900

    feat :: 검색 추가

--- src/app/(page)/(with-nav)/explore/page.tsx
+++ src/app/(page)/(with-nav)/explore/page.tsx
@@ -4,6 +4,10 @@ import MainContainer from "@/shared/components/containers/MainContainer";
 import AdvancedSearchBar from "@/features/explore/components/AdvancedSearchBar";
 import MiniSuggestList from "@/features/explore/components/MiniSuggestList";
 import LocalWeather from "@/features/explore/components/LocalWeather";
+import Title from "@/features/schedule/user/components/Title";
+import TextInput from "@/shared/components/inputs/TextInput";
+import Search from "@public/icons/search.svg";
+import {usePlaceQuery} from "@/features/schedule/user/hooks/usePlaceQuery";
 
 const SuggestType: string[] = [
   "관광지",
@@ -43,11 +47,58 @@ export default function Page() {
 
   // SuggestType에서 중복 없이 3개 뽑기 (첫 렌더 시 고정)
   const randomTypes = useMemo(() => getRandomUnique(SuggestType, 3), []);
+  const [input, setInput] = useState<string>("");
+  const [debouncedInput, setDebouncedInput] = useState("");
 
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedInput(input);
+    }, 400);
+    return () => clearTimeout(handler);
+  }, [input]);
+
+  const { data, isLoading, isError } = usePlaceQuery(
+      debouncedInput,
+      coords?.longitude ?? undefined,
+      coords?.latitude ?? undefined,
+      !!debouncedInput
+  );
   return (
     <MainContainer>
       <LocalWeather />
-      <AdvancedSearchBar />
+      <AdvancedSearchBar onChange={e => setInput(e.target.value)} />
+        <div className="flex flex-col gap-4 px-4 py-2 w-full">
+            {isLoading && (
+                <div className="text-center text-gray-400 py-4">로딩중...</div>
+            )}
+            {isError && (
+                <div className="text-center text-red-400 py-4">에러 발생</div>
+            )}
+            {data && (
+                <button
+                    type="button"
+                    className={`flex gap-3 items-center w-full border rounded-xl p-2 transition-colors border-transparent`}
+                >
+                    <div className="overflow-clip relative shrink-0 w-[100px] h-[100px]">
+                        <div
+                            className="absolute inset-0 rounded-[12px] bg-cover bg-center"
+                            style={{ backgroundImage: `url('${data.image}')` }}
+                        />
+                    </div>
+                    <div className="flex flex-col gap-1 items-start justify-center text-left">
+                        <div className="font-medium text-[#111] text-[18px] leading-6">
+                            {data.place_name}
+                        </div>
+                        <div className="font-light text-[#aaa] text-[12px] leading-4">
+                            {data.address}
+                        </div>
+                        <div className="font-light text-[#aaa] text-[12px] leading-4">
+                            평점: {data.rating} / 북마크: {data.bookmark_cnt} / 거리: {data.distance}m
+                        </div>
+                    </div>
+                </button>
+            )}
+        </div>
       {coords && (
         <>
           {randomTypes.map((type, i) => (
--- src/features/explore/components/AdvancedSearchBar.tsx
+++ src/features/explore/components/AdvancedSearchBar.tsx
@@ -5,11 +5,14 @@ import TextInput from "@/shared/components/inputs/TextInput";
 import Search from "@public/icons/search.svg";
 import Select from "@/shared/components/inputs/Select";
 
-const AdvancedSearchBar = () => {
+interface AdvencedSearchBarProps {
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+const AdvancedSearchBar: React.FC<AdvencedSearchBarProps> = ({onChange}) => {
   const Category: string[] = ["a", "b", "c", "d"];
   return (
     <SectionContainer className="flex-wrap gap-3">
-      <TextInput src={Search} alt={"Place"} placeholder={"장소를 검색해보세요"} />
+      <TextInput src={Search} alt={"Place"} placeholder={"장소를 검색해보세요"} onChange={onChange} />
       <div className="flex gap-3">
         <Select placeholder="카테고리" items={Category}/>
         <Select placeholder="가격대" items={Category}/>
