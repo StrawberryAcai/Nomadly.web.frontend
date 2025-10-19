cbe878979b76b383fd8555e4ec0b4556edad0750
bitbyte08 <me@bitworkspace.kr>
2025-10-19 14:55:55 +0900

    feat :: 마이페이지 api 연동

--- src/app/(page)/(with-nav)/profile/(with-nav)/bookmark/places/page.tsx
+++ src/app/(page)/(with-nav)/profile/(with-nav)/bookmark/places/page.tsx
@@ -1,6 +1,12 @@
+'use client';
 import MainContainer from "@/shared/components/containers/MainContainer";
+import {useEffect} from "react";
+import axiosInstance from "@/shared/lib/axiosInstance";
 
 export default function Page() {
+  useEffect(()=>{
+    axiosInstance.get("/api/me/bookmark/place").then((value)=>console.log(value.data));
+  })
   return (
     <MainContainer>
       2
