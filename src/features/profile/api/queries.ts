15cdb9f1c081630ee2a8d55ce06f56b9bff48031
bitbyte08 <me@bitworkspace.kr>
2025-10-24 00:07:35 +0900

    feat :: 목데이터 제거

--- src/features/profile/api/queries.ts
+++ src/features/profile/api/queries.ts
@@ -16,8 +16,8 @@ export const getMePlan = async (): Promise<PlanListResponse[]> => {
 }
 
 export const getMeBookmarkedPlan = async (): Promise<BookmarkResponse> => {
-  // await api.get<BookmarkResponse>("/api/me/bookmark/plans");
-  return dummyBookmarkData
+  const res = await api.get<BookmarkResponse>("/api/me/bookmark/plans");
+  return res.data;
 }
 
 export const getMePublicPlan = async (): Promise<PlanListResponse[]> => {
