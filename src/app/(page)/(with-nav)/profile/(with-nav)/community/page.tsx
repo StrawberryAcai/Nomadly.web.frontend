e67e808c8ec3c28bc52722df1ea676ed0e7e2e4a
bitbyte08 <me@bitworkspace.kr>
2025-10-24 07:58:05 +0900

    feat :: API 테스트 코드 추가

--- src/app/(page)/(with-nav)/profile/(with-nav)/community/page.tsx
+++ src/app/(page)/(with-nav)/profile/(with-nav)/community/page.tsx
@@ -1,8 +1,14 @@
+"use client";
 import {getMeBookmarkedPlan} from "@/features/profile/api/queries";
 import MainContainer from "@/shared/components/containers/MainContainer";
 import PlanList from "@/shared/components/plan/PlanList";
+import api from "@/shared/lib/axiosInstance";
+import { useEffect } from "react";
 
-export default async function Page() {
+export default function Page() {
+  useEffect(()=> {
+    console.log(api.get("/api/me/like/board"));
+  })
   return (
     <MainContainer>
       temp
