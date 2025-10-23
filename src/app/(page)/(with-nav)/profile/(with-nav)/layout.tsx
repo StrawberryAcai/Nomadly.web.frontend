440789300e6921e582b89f3e5aaa1ee6a8bd91e9
bitbyte08 <me@bitworkspace.kr>
2025-10-24 00:10:59 +0900

    feat :: 로그인 여부에 따라 렌더링

--- src/app/(page)/(with-nav)/profile/(with-nav)/layout.tsx
+++ src/app/(page)/(with-nav)/profile/(with-nav)/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import TopBar from "@/shared/components/layout/TopBar";
 import Image from "next/image";
@@ -6,16 +7,22 @@ import ProfileCard from "@/features/profile/components/ProfileCard";
 import MainContainer from "@/shared/components/containers/MainContainer";
 import NavBar from "@/features/profile/components/NavBar";
 import Link from "next/link";
+import { useProfileQuery } from "@/features/profile/hooks/useProfileQuery";
 
-export default function layout({children}: {children: React.ReactNode}) {
+export default function Layout({children}: {children: React.ReactNode}) {
+  const {data} = useProfileQuery();
   return (
     <MainContainer>
       <TopBar>
         <Link href="/profile/settings"><Image src={Setting} alt="setting" /></Link>
       </TopBar>
       <ProfileCard />
-      <NavBar />
-      {children}
+      {data && 
+      <>
+        <NavBar />
+        {children}
+      </>
+      }
     </MainContainer>
   )
 }
