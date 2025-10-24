8c1f2493359074d5f048f006a23c95b670fe2216
BitByte08 <me@bitworkspace.kr>
2025-10-25 01:57:53 +0900

    fix :: Bookmark 문제 수정

--- src/features/profile/api/queries.ts
+++ src/features/profile/api/queries.ts
@@ -16,6 +16,7 @@ export const getMePlan = async (): Promise<MyPlan[]> => {
 }
 
 export const getMeBookmarkedPlan = async (): Promise<BookmarkResponse> => {
+    return dummyBookmarkData;
   const res = await api.get<BookmarkResponse>("/api/me/bookmark/place");
   console.log(res.data);
   return res.data;
